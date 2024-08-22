a8b0d3a739e90eeb7cccc34f36b7ed9af71f33e8
lucas_c_leandro <lucas_c_leandro@SN-357864>
2024-08-22 09:10:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorMoedas/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ConversorMoedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo1 = new System.Windows.Forms.ComboBox();
+            this.combo2 = new System.Windows.Forms.ComboBox();
+            this.txbNumConvert = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblConvertido = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // combo1
+            // 
+            this.combo1.FormattingEnabled = true;
+            this.combo1.Items.AddRange(new object[] {
+            "Real",
+            "Dolar",
+            "Euro",
+            "Pesos Argentinos"});
+            this.combo1.Location = new System.Drawing.Point(273, 170);
+            this.combo1.Name = "combo1";
+            this.combo1.Size = new System.Drawing.Size(121, 24);
+            this.combo1.TabIndex = 0;
+            // 
+            // combo2
+            // 
+            this.combo2.FormattingEnabled = true;
+            this.combo2.Items.AddRange(new object[] {
+            "Real",
+            "Dolar",
+            "Euro",
+            "Pesos Argentinos"});
+            this.combo2.Location = new System.Drawing.Point(273, 233);
+            this.combo2.Name = "combo2";
+            this.combo2.Size = new System.Drawing.Size(121, 24);
+            this.combo2.TabIndex = 1;
+            // 
+            // txbNumConvert
+            // 
+            this.txbNumConvert.Location = new System.Drawing.Point(273, 109);
+            this.txbNumConvert.Name = "txbNumConvert";
+            this.txbNumConvert.Size = new System.Drawing.Size(121, 22);
+            this.txbNumConvert.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor para converter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Converter de:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(189, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Para:";
+            // 
+            // lblConvertido
+            // 
+            this.lblConvertido.AutoSize = true;
+            this.lblConvertido.Location = new System.Drawing.Point(191, 296);
+            this.lblConvertido.Name = "lblConvertido";
+            this.lblConvertido.Size = new System.Drawing.Size(108, 16);
+            this.lblConvertido.TabIndex = 6;
+            this.lblConvertido.Text = "Valor convertido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(116, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 36);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Conversor de moedas";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(422, 108);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 374);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblConvertido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbNumConvert);
+            this.Controls.Add(this.combo2);
+            this.Controls.Add(this.combo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combo1;
+        private System.Windows.Forms.ComboBox combo2;
+        private System.Windows.Forms.TextBox txbNumConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblConvertido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ ConversorMoedas/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorMoedas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Converte o valor digitado no TextBox para um número inteiro
+                double i = double.Parse(txbNumConvert.Text);
+
+                // Verifica qual moeda foi selecionada no combo1 e combo2 e realiza a conversão
+                if (combo1.SelectedItem.ToString() == "Real" && combo2.SelectedItem.ToString() == "Dolar")
+                {
+                    double result = i / 5; // Supondo que 1 Dólar = 5 Reais
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Real" && combo2.SelectedItem.ToString() == "Euro")
+                {
+                    double result = i / 5.5; // Supondo que 1 Euro = 5.5 Reais
+                    lblConvertido.Text = "Valor convertido: €" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Real" && combo2.SelectedItem.ToString() == "Pesos Argentinos")
+                {
+                    double result = i * 70; // Supondo que 1 Real = 70 Pesos Argentinos
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2") + " ARS";
+                }
+                else if (combo1.SelectedItem.ToString() == "Dolar" && combo2.SelectedItem.ToString() == "Real")
+                {
+                    double result = i * 5; // Supondo que 1 Dólar = 5 Reais
+                    lblConvertido.Text = "Valor convertido: R$" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Dolar" && combo2.SelectedItem.ToString() == "Euro")
+                {
+                    double result = i * 0.91; // Supondo que 1 Dólar = 0.91 Euros
+                    lblConvertido.Text = "Valor convertido: €" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Dolar" && combo2.SelectedItem.ToString() == "Pesos Argentinos")
+                {
+                    double result = i * 350; // Supondo que 1 Dólar = 350 Pesos Argentinos
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2") + " ARS";
+                }
+                else if (combo1.SelectedItem.ToString() == "Euro" && combo2.SelectedItem.ToString() == "Real")
+                {
+                    double result = i * 5.5; // Supondo que 1 Euro = 5.5 Reais
+                    lblConvertido.Text = "Valor convertido: R$" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Euro" && combo2.SelectedItem.ToString() == "Dolar")
+                {
+                    double result = i / 0.91; // Supondo que 1 Euro = 1.1 Dólares
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Euro" && combo2.SelectedItem.ToString() == "Pesos Argentinos")
+                {
+                    double result = i * 385; // Supondo que 1 Euro = 385 Pesos Argentinos
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2") + " ARS";
+                }
+                else if (combo1.SelectedItem.ToString() == "Pesos Argentinos" && combo2.SelectedItem.ToString() == "Real")
+                {
+                    double result = i / 70; // Supondo que 70 Pesos Argentinos = 1 Real
+                    lblConvertido.Text = "Valor convertido: R$" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Pesos Argentinos" && combo2.SelectedItem.ToString() == "Euro")
+                {
+                    double result = i / 385; // Supondo que 385 Pesos Argentinos = 1 Euro
+                    lblConvertido.Text = "Valor convertido: €" + result.ToString("N2");
+                }
+                else if (combo1.SelectedItem.ToString() == "Pesos Argentinos" && combo2.SelectedItem.ToString() == "Dolar")
+                {
+                    double result = i / 350; // Supondo que 350 Pesos Argentinos = 1 Dólar
+                    lblConvertido.Text = "Valor convertido: $" + result.ToString("N2");
+                }
+                else
+                {
+                    lblConvertido.Text = "Selecione uma conversão válida.";
+                }
+            }
+            catch (FormatException)
+            {
+                lblConvertido.Text = "Por favor, insira um número válido.";
+            }
+        }
+    }
+}
